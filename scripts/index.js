773df87992c9d28d7d9674f3322cd8a7d4094236
NovitskiyAleksei <mr.novik@gmail.com>
2013-09-17 00:11:06 +0300

    Loaded single image, added robot picture

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
 var canvas = null,
-    context = null;
+    context = null
+    img = null;
 
-setup = function(){
+var onImageLoad = function(){
+    context.drawImage()
+}
+
+var setup = function(){
     canvas = document.getElementById('canvas')
     context = canvas.getContext('2d')
     canvas.width = 1200
     canvas.height = 720
 
+    img = new Image();
+    img.onload = onImageLoad;
+    img.src = "/media/img/gamedev/ralphyrobot.png";
 }
 
 setup()
